9a9510c5204fb20f5b5c01f6e3913b8a88dacfd9
praneethch0308 <153581733+praneethch0308@users.noreply.github.com>
2024-05-30 11:34:01 +0530

    Update db.js

--- passwordmanager/backend/db.js
+++ passwordmanager/backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const mongoURL = "mongodb+srv://praneethchilukuri03:safevault123@safevault.yzfd3fh.mongodb.net/passmanager"
+const mongoURL = "YOUR_URL"
 
 const connectToMongo = () =>{
     mongoose.connect(mongoURL).then(()=>console.log("Connected")).catch((e)=>console.log(e.message))
